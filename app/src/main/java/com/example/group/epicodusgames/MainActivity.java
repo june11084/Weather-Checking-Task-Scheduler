03fdfa407bda9b30e0cc4117104240f0a089d0ca
Jun Li <june11084@gmail.com>
2018-03-16 10:37:04 -0700

    add main activity buttons to start other activities

--- app/src/main/java/com/example/group/epicodusgames/MainActivity.java
+++ app/src/main/java/com/example/group/epicodusgames/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.group.epicodusgames;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private static final String TAG = "MyActivity";
+    @BindView(R.id.playGames) Button mPlayGames;
+    @BindView(R.id.checkWeather) Button mCheckWeather;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mPlayGames.setOnClickListener(this);
+        mCheckWeather.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mPlayGames){
+            Intent intent = new Intent(MainActivity.this, SelectGameActivity.class);
+            intent.putExtra("message", "you are playing games");
+            startActivity(intent);
+        } else if (v == mCheckWeather){
+            Toast.makeText(this,"Not Implemented",Toast.LENGTH_LONG).show();
+        }
     }
 }
