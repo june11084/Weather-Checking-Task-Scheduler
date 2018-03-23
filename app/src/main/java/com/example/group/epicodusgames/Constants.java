351d3b234da18aa5f4541845088d0f3bafaf2839
Jun Li <june11084@gmail.com>
2018-03-23 15:40:27 -0700

    untrack ignored files

--- app/src/main/java/com/example/group/epicodusgames/Constants.java
+++ app/src/main/java/com/example/group/epicodusgames/Constants.java
@@ -5,5 +5,4 @@ public class Constants {
     public static final String OPENWEATHER_BASE_URL = "api.openweathermap.org/data/2.5/weather?zip=06066,us";
     public static final String OPENWEATHER_ZIP_QUERY_PARAMETER = "zip";
     public static final String OPENWEATHER_API_KEY_QUERY_PARAMETER = "appid";
-
 }
