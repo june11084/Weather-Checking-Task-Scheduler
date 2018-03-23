46e73eafc7f4ad3b8b75ef840ceb2737d70fc145
Jun Li <june11084@gmail.com>
2018-03-23 14:19:41 -0700

    add alarmClock adapter, fragment, and fragment_page.xml

---
+++ app/src/main/java/com/example/group/epicodusgames/adapters/AlarmClockAdapter.java
@@ -0,0 +1,42 @@
+package com.example.group.epicodusgames.adapters;
+
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import com.example.group.epicodusgames.ui.AlarmClockPageFragment;
+
+
+/**
+ * Created by june1 on 3/23/2018.
+ */
+
+public class AlarmClockAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 3;
+    private String tabTitles[] = new String[] { "Tab1", "Tab2", "Tab3" };
+    private Context context;
+
+    public AlarmClockAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return AlarmClockPageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+
+}
--- app/src/main/java/com/example/group/epicodusgames/ui/WeatherAlarmClockActivity.java
+++ app/src/main/java/com/example/group/epicodusgames/ui/WeatherAlarmClockActivity.java
@@ -1,15 +1,26 @@
 package com.example.group.epicodusgames.ui;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TabLayout;
 import android.os.Bundle;
 
 import com.example.group.epicodusgames.R;
+import com.example.group.epicodusgames.adapters.AlarmClockAdapter;
+
+import butterknife.BindView;
 
 public class WeatherAlarmClockActivity extends AppCompatActivity {
+    @BindView(R.id.viewpager) ViewPager mViewPager;
+    @BindView(R.id.sliding_tabs) TabLayout MSliding_Tabs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_weather);
+        setContentView(R.layout.activity_weather_alarm_clock);
+
+        mViewPager.setAdapter(new AlarmClockAdapter(getSupportFragmentManager(),
+                WeatherAlarmClockActivity.this));
+        MSliding_Tabs.setupWithViewPager(mViewPager);
     }
 }
