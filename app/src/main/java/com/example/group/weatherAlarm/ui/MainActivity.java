c9071a927565ca0428c05901b86a3193d7aa190f
Jun Li <june11084@gmail.com>
2018-03-30 15:41:49 -0700

    add logout menu and methods to go to loginActivity

--- app/src/main/java/com/example/group/weatherAlarm/ui/MainActivity.java
+++ app/src/main/java/com/example/group/weatherAlarm/ui/MainActivity.java
@@ -5,12 +5,16 @@ import android.graphics.Typeface;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.group.weatherAlarm.R;
 import com.example.group.weatherAlarm.services.WeatherService;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.io.IOException;
 
@@ -55,6 +59,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://openweathermap.org/city/5746545"));
             startActivity(webIntent);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            logout();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        startActivity(new Intent(getBaseContext(), LoginActivity.class));
     }
 
     private void getWeather(String zip) {
